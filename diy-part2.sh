078385bd7aa37c06b769496dd065f9d35dc4625d
xmbay <75921952+xmbay@users.noreply.github.com>
2023-02-14 10:45:30 +0800

    Update diy-part2.sh

--- diy-part2.sh
+++ diy-part2.sh
@@ -11,4 +11,13 @@
 #
 
 # Modify default IP
-#sed -i 's/192.168.1.1/192.168.50.5/g' package/base-files/files/bin/config_generate
+sed -i 's/192.168.1.1/192.168.2.1/g' package/base-files/files/bin/config_generate
+
+echo '修改主机名'
+sed -i "s/hostname='OpenWrt'/hostname='Nighthawk-X4'/g" package/base-files/files/bin/config_generate
+cat package/base-files/files/bin/config_generate |grep hostname=
+echo '=========Alert hostname OK!========='
+
+# Change Default Theme
+sed -i 's/Bootstrap/Argon/g' feeds/luci/collections/luci/Makefile
+sed -i 's/-bootstrap/-argon/g' feeds/luci/collections/luci/Makefile
